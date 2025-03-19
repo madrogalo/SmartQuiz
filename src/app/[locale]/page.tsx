9e43cc8d424de2249d0b826531afcce0c8080235
Ievhenii <youn4ik@gmail.com>
2025-03-20 01:21:29 +0200

    add start quiz button

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,18 @@
 "use client";
-
+import { useRouter } from "next/navigation";
+import { Button } from "./components/Button";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const router = useRouter();
+  const handleStartQuiz = () => {
+    router.push("/en/quiz/1");
+  };
   return (
     <div className={styles.page}>
-      <div className={styles.content}>text</div>
+      <div className={styles.content}>
+        <Button onClick={handleStartQuiz}>Start Quiz</Button>
+      </div>
     </div>
   );
 }
